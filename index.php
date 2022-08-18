e15157df52411ccc75ff19266d37217337fee1b9
tbeatzz <tbeatzz20k@gmail.com>
2022-08-18 18:19:03 -0300

    primer commit
    home parallax

--- index.php
+++ index.php
@@ -4,36 +4,111 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <title>Ejemplo</title>
-        <base href="http://localhost/ProyectoEjemplo/"/>
         <!--Estilos-->
         <link href="lib/bootstrap.min.css" rel="Stylesheet" type="text/css" /> 
-        <link href="/css/estilos.css" type="text/css" />
+        <link rel="stylesheet" href="./css/main.css">
         <!--Scripts-->
         <script  src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>  
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-        <link href="/lib/bootstrap.min.js" type="text/javascript"/>
+        <link href="/lib/boostrap.min.css" type="text/javascript"/>
         <script src="lib/bootstrap.bundle.min.js" type="text/javascript"></script>
+        
         
         
     </head>
     <body>
-        <!-- Menu colapsable -->
-        <nav class="navbar navbar-expand-lg navbar-light bg-dark">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-              <div class="navbar-nav">
-                <a class="nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-                <a class="nav-link" href="#">Features</a>
-                <a class="nav-link" href="#">Pricing</a>
-                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-              </div>
+        
+      <div class="app">
+        
+        <div class="app-wrapper">
+
+          <div class="app-content">
+
+            <div class="parallax" style="background-image: url(./assets/img/eico.jpg); "></div>
+            <div id="contenidoParallax">
+                <div class="container" style="margin: auto;text-align: center; " id="containerParallax">
+                        <br/>
+                        <br/>
+                        
+                    <h2> Cancha de Cesped Sintético </h2>
+                    
+                    <div id="textoParallax" class="col-md-10 col-md-offset-1">
+                        <p>
+                        Nuestro moderno complejo ofrece el servicio de alquiler de canchas de futbol 5. Contamos con 3 canchas de césped sintético con capacidad para jugar entre 10 y 12 jugadores.
+                        <br>  
+                        </p>
+                        <br>
+                        <p> <strong> Valor: </strong> $300 por hora </p>
+                    </div>
+                
+                </div>
             </div>
-         </nav>
-        
-        
-            
+
+            <div class="parallax" style="background-image: url(./assets/img/eico2.jpg); "></div>
+            <div id="contenidoParallax">
+                <div class="container" style="margin: auto;text-align: center; " id="containerParallax">
+                        <br/>
+                        <br/>
+                        
+                    <h2> Cancha de Cesped Sintético </h2>
+                    
+                    <div id="textoParallax" class="col-md-10 col-md-offset-1">
+                        <p>
+                        Nuestro moderno complejo ofrece el servicio de alquiler de canchas de futbol 5. Contamos con 3 canchas de césped sintético con capacidad para jugar entre 10 y 12 jugadores.
+                        <br>  
+                        </p>
+                        <br>
+                        <p> <strong> Valor: </strong> $300 por hora </p>
+                    </div>
+                
+                </div>
+            </div>
+
+            <div class="parallax" style="background-image: url(./assets/img/eico3.jpg); "></div>
+            <div id="contenidoParallax">
+                <div class="container" style="margin: auto;text-align: center; " id="containerParallax">
+                        <br/>
+                        <br/>
+                        
+                    <h2> Cancha de Cesped Sintético </h2>
+                    
+                    <div id="textoParallax" class="col-md-10 col-md-offset-1">
+                        <p>
+                        Nuestro moderno complejo ofrece el servicio de alquiler de canchas de futbol 5. Contamos con 3 canchas de césped sintético con capacidad para jugar entre 10 y 12 jugadores.
+                        <br>  
+                        </p>
+                        <br>
+                        <p> <strong> Valor: </strong> $300 por hora </p>
+                    </div>
+                
+                </div>
+            </div>
+
+            <div class="parallax" style="background-image: url(./assets/img/eico4.jpg); "></div>
+            <div id="contenidoParallax">
+                <div class="container" style="margin: auto;text-align: center; " id="containerParallax">
+                        <br/>
+                        <br/>
+                        
+                    <h2> Cancha de Cesped Sintético </h2>
+                    
+                    <div id="textoParallax" class="col-md-10 col-md-offset-1">
+                        <p>
+                        Nuestro moderno complejo ofrece el servicio de alquiler de canchas de futbol 5. Contamos con 3 canchas de césped sintético con capacidad para jugar entre 10 y 12 jugadores.
+                        <br>  
+                        </p>
+                        <br>
+                        <p> <strong> Valor: </strong> $300 por hora </p>
+                    </div>
+                
+                </div>
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+    
     </body>
 </html>
