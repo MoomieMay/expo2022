6967cb0b7bdc721079557119f3bef6a59188a6bd
tbeatzz <tbeatzz20k@gmail.com>
2022-10-14 17:19:06 -0300

    pagina proyectos
    dise;o de pagina de proyectos

--- pages/proyectos/main.js
+++ pages/proyectos/main.js
@@ -38,3 +38,24 @@ $(document).ready(function () {
     $(".bar").removeClass("active");
   });
 });
+$("html, body").animate(
+  {
+    scrollTop: $("div").offset().top,
+  },
+  550
+);
+
+var lastScrollTop = 0;
+$(window).scroll(function (event) {
+  var st = $(this).scrollTop();
+  if (st > lastScrollTop) {
+    //âíèç
+    $(".btn__container").addClass("scrolling_down");
+    $(".btn__container").removeClass("scrolling_up");
+  } else {
+    // ââåðõ
+    $(".btn__container").addClass("scrolling_up");
+    $(".btn__container").removeClass("scrolling_down");
+  }
+  lastScrollTop = st;
+});
