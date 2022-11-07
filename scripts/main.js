eb872a24e1183497ef142bbd7983ab9b194d6cb2
tbeatzz <tbeatzz20k@gmail.com>
2022-11-07 15:49:43 -0300

    template cambios finales

--- scripts/main.js
+++ scripts/main.js
@@ -121,3 +121,32 @@ function animate() {
 
 // Start the animation.
 window.onload = animate();
+
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+    mybutton.style.width = "50px";
+    mybutton.style.opacity = "1";
+    mybutton.style.zIndex = "99999";
+    mybutton.style.pointerEvents = "auto";
+  } else {
+    mybutton.style.width = "0";
+    mybutton.style.zIndex = "-1";
+    mybutton.style.opacity = "0";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
